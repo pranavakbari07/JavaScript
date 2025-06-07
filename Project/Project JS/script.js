5a7d92927dcc4f6d921599e72597bd05bd03a98e
pranavakbari07 <pranavakbari07@gmail.com>
2025-06-07 10:57:38 +0530

    project

---
+++ Project/Project JS/script.js
@@ -0,0 +1,25 @@
+
+console.log("Hello JS")
+console.warn("This is a Warninng")
+console.error("This is a error")
+
+alert("This is an alert")
+confirm("This is a confirmation box")
+prompt("Enter your name : ")
+
+let a = 30;
+let b = 20;
+console.log(a+b);
+
+let first = prompt("Enter your First Name : ");
+let last = prompt("Enter your Last Name : ");
+console.log(first+" "+last);
+
+let year = prompt("Enter your born year : ");
+alert(2025-year);
+
+// // Template literals
+// // ``backtick
+let naam = "Jensi";
+let str = `My name is ${naam}`;
+console.log(str);
